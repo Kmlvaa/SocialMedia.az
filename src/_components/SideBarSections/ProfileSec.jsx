98482e263fe9bbf9858b5ee99c2e596cba951b86
Kmlvaa <samiraak@code.edu.az>
2025-07-16 00:48:01 +0400

    user info on home page

--- src/_components/SideBarSections/ProfileSec.jsx
+++ src/_components/SideBarSections/ProfileSec.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import useUser from '../../hooks/useUser'
 
 export default function SidebarProfileSec() {
+
+    const { user, loading } = useUser();
     return (
         <section className='Profile-info flex flex-col items-center justify-center'>
             <div className='rounded-full border-4 border-[#2c2c2c] w-16 h-16'>
                 <img src='https://img.freepik.com/premium-vector/man-professional-business-casual-young-avatar-icon-illustration_1277826-623.jpg?semt=ais_hybrid&w=740' alt='Profile Photo' className='rounded-full object-cover' />
             </div>
             <div className='text-center'>
-                <h2>Jessica Cambridge</h2>
+                {user ? (
+                    <h2>{user.firstName} {user.lastName}</h2>
+                ) : <h2>Jhon Wilston</h2>}
                 <p className='text-xs text-[#5b5b5c]'>Software Engineer</p>
                 <p className='py-2'>Guiding the next generation through the journey of health and knowledge</p>
                 <div className='flex flex-row items-center justify-center gap-5 w-full'>
